3d94288577f33b81b57d6197ae0bd2bdde699f21
Wesley <wfrancescons@gmail.com>
2022-07-30 17:19:57 -0300

    enable graceful stop

--- src/bot.js
+++ src/bot.js
@@ -28,3 +28,6 @@ if (environment === 'development') {
 console.log(`Running in ${environment} environment`)
 
 bot.launch()
+
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
