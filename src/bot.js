af49b77362a4e98299a4405c09e7ceb0c638bd0c
Wesley <wfrancescons@gmail.com>
2022-07-30 18:39:47 -0300

    deploy

--- src/bot.js
+++ src/bot.js
@@ -2,7 +2,10 @@ if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 const environment = process.env.NODE_ENV
 const token = process.env.TELEGRAM_BOT_TOKEN
 const { Telegraf } = require('telegraf')
-const commands = require('./commands')
+const ln = require('./commands/ln')
+const alb = require('./commands/alb')
+const art = require('./commands/art')
+const reg = require('./commands/reg')
 
 if (token === undefined) {
   throw new Error('TELEGRAM_BOT_TOKEN must be provided!')
@@ -14,10 +17,10 @@ require('./database/')
 const bot = new Telegraf(token)
 
 // Set bot response
-bot.command('ln', (ctx) => commands.ln(ctx))
-bot.command('alb', (ctx) => commands.alb(ctx))
-bot.command('art', (ctx) => commands.art(ctx))
-bot.command('reg', (ctx) => commands.reg(ctx))
+bot.command('ln', (ctx) => ln(ctx))
+bot.command('alb', (ctx) => alb(ctx))
+bot.command('art', (ctx) => art(ctx))
+bot.command('reg', (ctx) => reg(ctx))
 
 if (environment === 'development') {
   require('http')
--- src/commands/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    ln: require('./ln'),
-    alb: require('./alb'),
-    art: require('./art'),
-    reg: require('./reg')
-}
