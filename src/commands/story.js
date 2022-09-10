48b0a675ad91d5c1c64c0cb3bc091e092db0272f
Wesley <wfrancescons@gmail.com>
2022-09-10 00:45:50 -0300

    refactor and performance improvements

--- src/commands/story.js
+++ src/commands/story.js
@@ -5,63 +5,86 @@ const makeStory = require('../controller/collage/collage')
 const acceptedMediaTypes = ['tracks', 'albums', 'artists']
 const acceptedPeriods = ['overall', '7day', '1month', '3month', '6month', '12month']
 
-const periodText = [
-    { period: 'overall', text: 'all time' },
-    { period: '7day', text: 'last 7 days' },
-    { period: '1month', text: 'last month' },
-    { period: '3month', text: 'last 3 months' },
-    { period: '6month', text: 'last 6 months' },
-    { period: '12month', text: 'last 12 months' },
-]
+const periodMap = {
+    'overall': 'all time',
+    '7day': 'last 7 days',
+    '1month': 'last month',
+    '3month': 'last 3 months',
+    '6month': 'last 6 months',
+    '12month': 'last 12 months'
+}
+
+const getLastfmData = (lastfm_user, media, period) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+
+            let data
+            if (media === 'tracks') data = await getUserTopTracks(lastfm_user, period)
+            if (media === 'albums') data = await getUserTopAlbums(lastfm_user, period)
+            if (media === 'artists') data = await getUserTopArtists(lastfm_user, period)
+            resolve(data)
+
+        } catch (erro) {
+            reject(erro)
+        }
+    })
+}
 
 const story = async (ctx) => {
 
     const { first_name } = ctx.update.message.from
     const text = ctx.update.message.text.trim().toLowerCase()
-    const [command, mediaType, period] = text.split(' ')
+    const [command, media_type, period] = text.split(' ')
 
-    if (!acceptedMediaTypes.includes(mediaType) || !acceptedPeriods.includes(period)) {
+    if (!acceptedMediaTypes.includes(media_type) || !acceptedPeriods.includes(period)) {
+
         ctx.replyWithChatAction('typing')
-        const invalidArgummentsText = 'Invalid argumments 🤔\n'+
-                                      'Type `/story mediatype period` to generate your collage.\n'+
-                                      '➡️ Example: `/story tracks 7day`\n\n'+
-                                      '✅ Valid MediaTypes: `tracks`, `albums`, `artists`\n'+
-                                      '✅ Valid Periods: `overall`, `7day`, `1month`, `3month`, `6month`, `12month`'
+
+        const invalidArgummentsText = 'Invalid argumments 🤔\n' +
+            'Type `/story mediatype period` to generate your collage.\n' +
+            '➡️ Example: `/story tracks 7day`\n\n' +
+            '✅ Valid MediaTypes: `tracks`, `albums`, `artists`\n' +
+            '✅ Valid Periods: `overall`, `7day`, `1month`, `3month`, `6month`, `12month`'
 
         return ctx.replyWithMarkdown(invalidArgummentsText)
     }
 
-    ctx.replyWithChatAction('upload_photo')
-
-    const formattedPeriod = periodText.find(item => item.period === period)
-
     try {
+        ctx.replyWithChatAction('typing')
+
         const lastfm_user = await getLastfmUser(ctx)
         if (!lastfm_user) return ctx.replyWithMarkdown('Type `/reg lastfmusername` to set your Lastfm\'s username')
 
-        let data
-        if (mediaType === 'tracks') {
-            data = await getUserTopTracks(lastfm_user, period)
-        }
-        if (mediaType === 'albums') {
-            data = await getUserTopAlbums(lastfm_user, period)
-        }
-        if (mediaType === 'artists') {
-            data = await getUserTopArtists(lastfm_user, period)
-        }
+        const response = await ctx.reply('🖼️ Generating your collage...')
+        const { message_id } = response
 
-        const model = {
-            period: formattedPeriod.text,
-            mediaType: mediaType,
-            data
-        }
-        const imageBuffer = await makeStory(model)
+        getLastfmData(lastfm_user, media_type, period)
+            .then(data => {
+                const periodToText = periodMap[period]
 
-        return ctx.replyWithPhoto({ source: imageBuffer })
+                const model = {
+                    period: periodToText,
+                    mediaType: media_type,
+                    data
+                }
+
+                ctx.replyWithChatAction('upload_photo')
+
+                makeStory(model).then(imageBuffer => {
+
+                    ctx.replyWithPhoto({ source: imageBuffer }, { caption: `${first_name}, your top ${media_type} of ${periodToText}` })
+                    ctx.deleteMessage(message_id)
+
+                })
+            })
+            .catch(erro => {
+                console.log(erro)
+                if (erro == "LastFm scrobbles is equal to zero") return ctx.replyWithMarkdown('There aren\'t any scrobbles on your lastFM. 🙁\n\nIs your username correct? 🤔\nType `/reg lastfmusername` to set your Lastfm\'s username')
+                return ctx.reply('Something went wrong 🥴 \nBut don\'t fret, let\'s give it another shot in a couple of minutes.')
+            })
 
     } catch (erro) {
         console.log(erro)
-        if (erro == "LastFm scrobbles is equal to zero") return ctx.replyWithMarkdown('There aren\'t any scrobbles on your lastFM. 🙁\n\nIs your username correct? 🤔\nType `/reg lastfmusername` to set your Lastfm\'s username')
         return ctx.reply('Something went wrong 🥴 \nBut don\'t fret, let\'s give it another shot in a couple of minutes.')
     }
 }
