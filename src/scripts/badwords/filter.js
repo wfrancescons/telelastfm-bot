588a597f877f4768107912eee2b39a8b078122af
Wesley Francescon da Silva <wfrancescons@gmail.com>
2022-12-06 22:46:43 -0300

    new badwords filter

--- src/scripts/badwords/filter.js
+++ src/scripts/badwords/filter.js
@@ -1,12 +1,20 @@
-import Filter from 'bad-words'
+import piii from 'piii'
 import badwords from './badwords.js'
 
-const filter = new Filter()
-
-filter.addWords(...badwords)
+const filter = new piii({
+    filters: [
+        badwords
+    ],
+    aliases: {
+        a: ['2', '4', '@'],
+        e: ['3', '&'],
+        o: ['0']
+    },
+    repeated: true
+})
 
 const hasBadword = text => {
-    return filter.isProfane(text)
+    return filter.has(text)
 }
 
 export { hasBadword }
