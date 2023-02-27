a265cb0135d294a3a8f37a43603873f4b9af0e56
Wesley Francescon da Silva <wfrancescons@gmail.com>
2023-02-27 20:44:43 -0300

    new cron time trigger

--- src/cron/cronjob.js
+++ src/cron/cronjob.js
@@ -3,7 +3,7 @@ import sendMessages from './sendMessages.js'
 import updateUserScrobbles from './updateUserScrobbles.js'
 import updateValidUsers from './updateValidUsers.js'
 
-export default Cron('0 30 20 * * 0', { timezone: "America/Sao_Paulo" }, async () => {
+export default Cron('0 30 19 * * 0', { timezone: "America/Sao_Paulo" }, async () => {
     try {
         console.log('CRONJOB: Task started')
         await updateValidUsers()
