f7b6e829d75630ba5de8779f4b5e061d8bdf1910
Wesley Francescon <wfrancescons@gmail.com>
2024-06-23 19:38:14 -0300

    try to fix web scrapping

--- src/services/lastfm.js
+++ src/services/lastfm.js
@@ -15,7 +15,7 @@ const apiLimiter = new Bottleneck({
 
 const webScrappingLimiter = new Bottleneck({
   maxConcurrent: 10,
-  minTime: 200
+  minTime: 100
 })
 
 function buildUrl(params) {
@@ -55,7 +55,15 @@ function extractImage(images) {
 
 async function getOgImage(url) {
   try {
-    const html = await request({ url })
+    const html = await request({
+      url, options: {
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+          'Accept-Language': 'en-US,en;q=0.5',
+        }
+      }
+    })
     console.log(html.status, html.statusText)
     const $ = cheerio.load(await html.text())
     const ogImage = $('meta[property="og:image"]').attr('content')
