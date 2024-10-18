8545760e47908a7f39068d45786af85ad1cee311
Wesley Francescon <wfrancescons@gmail.com>
2024-10-17 23:16:24 -0300

    darken dominant colors to improve text contrast

--- src/utils/getPredominatColor.js
+++ src/utils/getPredominatColor.js
@@ -3,13 +3,17 @@ import getColors from 'get-image-colors'
 async function getPredominantColor(url) {
     try {
         const colors = await getColors(url)
-        const rgb = colors[0].rgb()
-        return rgb
+        let color = colors[0]
+
+        if (color.luminance() > 0.7) {
+            color = color.darken(2)
+        }
+
+        return color.rgb()
     } catch (error) {
         console.error(error)
         return [14, 14, 14]
     }
-
 }
 
 export default getPredominantColor
