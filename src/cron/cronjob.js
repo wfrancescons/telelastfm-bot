98acb19482eb5307f50e7279cd3a45aaa36a741e
Wesley Francescon <wfrancescons@gmail.com>
2024-10-22 12:12:01 -0300

    format code

--- src/cron/cronjob.js
+++ src/cron/cronjob.js
@@ -7,12 +7,19 @@ import updateValidUsers from './updateValidUsers.js'
 // '0 12 * * *' - todo dia às 12h
 export default Cron('0 30 19 * * 0', { timezone: 'America/Sao_Paulo' }, async () => {
     try {
+
         console.log('CRONJOB: Task started')
+
         await updateValidUsers()
         await updateUserScrobbles()
         await sendMessages()
+
         console.log('CRONJOB: Task finished')
+
     } catch (error) {
+
         console.log(error)
+
     }
+
 })
